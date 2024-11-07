a4f0a03cc8274efb6d4fc79f817524e4402041c2
bowie <bjalelis@gmail.com>
2024-11-07 19:19:51 -0300

    feat: public route table

---
+++ igw.tf
@@ -0,0 +1,36 @@
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-igw"
+    }
+  )
+}
+
+resource "aws_route_table" "eks-public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-public-route-table"
+    }
+  )
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks-public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks-public_route_table.id
+}
