ebecd887311c31fe47400836fa6f19efdcd4e26c
bowie <bjalelis@gmail.com>
2024-11-07 15:57:20 -0300

    feat: adding remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.74.0"
     }
+  }
+  backend "s3" {
+    bucket = "com-devops"
+    key    = "tfstate"
+    region = "us-east-1"
   }
 }
 
