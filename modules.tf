7a62456bcce50a8f63e129905e2848931e517282
bowie <bjalelis@gmail.com>
2024-11-08 09:43:19 -0300

    feat: network module

--- modules.tf
+++ modules.tf
@@ -1,5 +1,5 @@
 module "eks_network" {
   source       = "./modules/network"
-  project_name = "mod-eks-network"
-  cidr_block   = "10.0.0.0/16"
+  project_name = var.project_name
+  cidr_block   = var.cidr_block
 }
